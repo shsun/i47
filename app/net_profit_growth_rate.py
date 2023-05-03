ef0743aa13a561392d60654e7749a446ed21d576
shanghai_sun <shanghai_sun@devops.com>
2023-05-03 15:05:24 +0800

    init

--- app/net_profit_growth_rate.py
+++ app/net_profit_growth_rate.py
@@ -2,5 +2,17 @@
 # coding=utf-8
 
 """
-公司利润年增长率>10%
+公司利润年增长率>10% =
+
+当年  net_profit	int	净利润
+
+
+TTM ????
+当年  net_profit_curr
+上年  net_profit_prev
+
+
+r = (net_profit_curr-net_profit_prev)/net_profit_prev
+
+
 """
