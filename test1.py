1b650ad1091ce1b1d6ac74b1c736e3075a4044be
shanghai_sun <shanghai_sun@devops.com>
2023-05-03 11:25:34 +0800

    init

--- test1.py
+++ test1.py
@@ -15,10 +15,12 @@ if ret == RET_OK:
 else:
     print('error:', data)
 quote_ctx.close()  # 关闭对象，防止连接条数用尽
+sys.exit(0)
 
-sys.exit(1)
 
-trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)  # 创建交易对象
-print(trd_ctx.place_order(price=500.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY,
-                          trd_env=TrdEnv.SIMULATE))  # 模拟交易，下单（如果是真实环境交易，在此之前需要先解锁交易密码）
-trd_ctx.close()  # 关闭对象，防止连接条数用尽
+# trd_ctx = OpenHKTradeContext(host='127.0.0.1', port=11111)  # 创建交易对象
+# print(trd_ctx.place_order(price=500.0, qty=100, code="HK.00700", trd_side=TrdSide.BUY,
+#                           trd_env=TrdEnv.SIMULATE))  # 模拟交易，下单（如果是真实环境交易，在此之前需要先解锁交易密码）
+# trd_ctx.close()  # 关闭对象，防止连接条数用尽
+# sys.exit(0)
+
--- test3.py
+++ test3.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# coding:utf-8
+
 from futu import *
 
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
@@ -10,3 +13,4 @@ if ret == RET_OK:
 else:
     print('error:', data)
 quote_ctx.close()  # 结束后记得关闭当条连接，防止连接条数用尽
+
