7b2613f72719ad7c575d41c1dac5446a236d6811
shanghai_sun <shanghai_sun@devops.com>
2023-05-03 11:53:00 +0800

    init

--- test3.py
+++ test3.py
@@ -2,21 +2,37 @@
 # coding:utf-8
 import datetime, time, os, sys, json, functools, random, sqlite3, greenlet
 from futu import *
+import pandas as pd
 
 
 def main():
     quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
 
-    ret, data = quote_ctx.get_ipo_list(Market.HK)
-    if ret == RET_OK:
-        print(data)
-        print(data['code'][0])  # 取第一条的股票代码
-        print(data['code'].values.tolist())  # 转为 list
-        print(len(data))
-    else:
-        print('error:', data)
+    success, df_hk = get_all_stocks_by_type(quote_ctx=quote_ctx, stock_type=Market.HK)
+    success, df_sh = get_all_stocks_by_type(quote_ctx=quote_ctx, stock_type=Market.SH)
+
     quote_ctx.close()  # 结束后记得关闭当条连接，防止连接条数用尽
     return 0
+
+
+def get_all_stocks_by_type(quote_ctx, stock_type=Market.HK):
+    """
+
+    :param quote_ctx:
+    :param stock_type:
+    :return:
+    """
+    ret_code, df = quote_ctx.get_stock_basicinfo(market=stock_type, stock_type=SecurityType.STOCK, code_list=None)
+    if ret_code == RET_OK:
+        print(df.shape[0], df.shape[1])
+        print(df.iloc[0][0])
+        success = True
+    else:
+        print('error', file=sys.stderr)
+        df = None
+        success = False
+
+    return success, df
 
 
 if __name__ == '__main__':
