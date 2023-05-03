9d2431b4d53ae55b1126003fd0d51856640cd6f9
shanghai_sun <shanghai_sun@devops.com>
2023-05-03 15:35:39 +0800

    init

--- app/charles/__init__.py
+++ app/charles/__init__.py
@@ -5,6 +5,9 @@ from app.charles.DYRFilter import TTMDYRFilter
 from app.charles.PBFilter import PBFilter
 from app.charles.PEFilter import PEFilter
 
-from app.charles.CharlesFilterChain import CharlesFilterChain
+from app.charles.filter_chains import CharlesFilterChain
 
 __all__ = ["CapFilter", "TTMDYRFilter", "PBFilter", "PEFilter", "CharlesFilterChain"]
+
+
+
